ac55e3ef3ac3eb7f7e54984cfd3ce1645a782bab
Ihor Sokolov <sihorjs@gmail.com>
2026-08-19 10:08:29 +0300

    add changes of entry point

--- gift_delivery_note_generator/main.py
+++ gift_delivery_note_generator/main.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 
 from gift_delivery_note_generator.services.document_parser import DocumentParser
 from gift_delivery_note_generator.services.document_reader import DocumentReader
-# from .test import PARSED_ORDER
+from gift_delivery_note_generator.services.delivery_note_renderer import DeliveryNoteRenderer
+
 
 if __name__ == "__main__":
     load_dotenv()
@@ -17,5 +18,10 @@ if __name__ == "__main__":
         scanned_document = document_reader.run()
         document_parser = DocumentParser(contents=scanned_document)
         parsed_data = document_parser.run()
+
+        for delivery_note in parsed_data:
+            renderer = DeliveryNoteRenderer(delivery_note=delivery_note)
+            renderer.run()
+
     except Exception as e:
         print(f"An error occurred: {e}")
